ce68701544f828f5e86f01f6a0ff7b7685e25804
Thibaud <thibaud@thibaud.gg>
2017-06-08 16:57:31 +0000

    exercice 1

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "#display_name when Bob admin" do
+     
+      user = User.new(admin:true,name:"Bob")
+     
+      assert_equal("Bob (admin)", user.display_name, msg = "nop")
+  end
+  
+  test "#display_name when Bob no admin" do
+     
+      user = User.new(admin:false,name:"Bob")
+     
+      assert_equal("Bob", user.display_name, msg = "nop")
+  end
+  
+
+ 
 end
